99ca34e8e3237ff079612c820febd53d23d0f3f5
Sananawaz32 <Sananawaz984@gmail.com>
2024-12-14 14:33:18 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+
+import Header from "@/components/Header";
+
+import Footer from "@/components/Footer";
+
+import Hero from "@/components/Hero";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <Hero />
+      </main>
+     
+      <Footer />
+    </div>
+  );
+}
